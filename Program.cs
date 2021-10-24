5865fa0d62123363d9d8645e43d94106436bc4cd
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-25 02:47:04 +0530

    Added a program where user can enter O or X

--- Program.cs
+++ Program.cs
@@ -4,15 +4,16 @@ namespace TicTacToeProject
 {
     class Program
     {
-        
-            static void Main(string[] args)
-            {
-                //TicTacToeGame game = new TicTacToeGame(); // Creating an object 'game' of class 'TicTacToeGame'
-                //game.TicTacToeGameMethod(); // Calling a method 'TicTacToeGameMethod'
 
-                TicTacToeGame game = new TicTacToeGame();
-                game.TicTacToeGameMethod(); // Calling a method 'TicTacToeGameMethod'
+
+        static void Main(string[] args)
+        {
+            //TicTacToeGame game = new TicTacToeGame(); // Creating an object 'game' of class 'TicTacToeGame'
+            //game.TicTacToeGameMethod(); // Calling a method 'TicTacToeGameMethod'
+
+            TicTacToeGame input = new TicTacToeGame();
+            input.InputLetterMethod();
         }
-        
+    
     }
 }
