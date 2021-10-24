466f0796e60133097555b8f0136c30f5166f791f
NoorMdKhan123 <noormd6400@gmail.com>
2021-10-25 01:59:34 +0530

    Welcome to Tic Tac Toe Program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TicTacToeProject
+{
+    class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                //TicTacToeGame game = new TicTacToeGame(); // Creating an object 'game' of class 'TicTacToeGame'
+                //game.TicTacToeGameMethod(); // Calling a method 'TicTacToeGameMethod'
+
+                TicTacToeGame input = new TicTacToeGame();
+                input.InputLetterMethod();
+            }
+        
+    }
+}
---
+++ TicTacToeGame.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToeProject
+{
+    class TicTacToeGame
+    {
+
+        // Declaring and creating an empty array named board of size 10 characters
+        char[] board = new char[10];
+
+        public char inpComputer;
+        public void InputLetterMethod()
+        {
+            Console.Write("Enter either X or O: ");
+            char inputLetter = Convert.ToChar(Console.ReadLine());
+            Console.Write("The user has entered " + inputLetter + "\n");
+            Random random = new Random();
+            int inpComp = random.Next(0, 2);
+
+            if (inpComp == 1)
+            {
+                inpComputer = 'X';
+            }
+            else
+            {
+                inpComputer = 'O';
+            }
+
+            Console.Write("The computer has entered " + inpComputer);
+            Console.ReadLine();
+
+        }
+    }
+}
